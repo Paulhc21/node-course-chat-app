586436a69ffdc86537019c5485187a32559a2177
Paul Canty <pcanty@greenough.com>
2017-12-27 21:53:11 -0800

    added socket.io to chat app

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const path = require( 'path' ),
+      http = require( 'http' ),
       express = require( 'express' ),
-      app = express();
+      app = express(),
+      socketIO = require( 'socket.io' );
 
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join( __dirname, '../public' );
 const port = process.env.PORT || 3000;
+const server = http.createServer( app );
+const io = socketIO( server );
 
-app.use(express.static(publicPath));
+app.use(express.static( publicPath ));
 
-// app.get('/', (req, res) => {
-//     res.render('test');
-// });
+io.on('connection', ( socket ) => {
+    console.log('New user connected');
 
-app.listen(port, () => {
+    socket.on('disconnect', ( reason ) => {
+        console.log(`Browser closed - ${ reason }`);
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server started on port ${ port }`);
 });
