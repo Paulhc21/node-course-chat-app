44f5c910c041c29bc642113f743ff7309071384f
Paul Canty <pcanty@greenough.com>
2017-12-28 20:38:50 -0800

    added emit for newMessage on createMessage for cross browser viewability

--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static( publicPath ));
 io.on('connection', ( socket ) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Paul',
-        text: 'Will pick up food from Traders',
-        createdAt: Date.now()
-    });
-
     socket.on('createMessage', ( message ) => {
         console.log('Message created', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', ( reason ) => {
