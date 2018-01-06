29ee45012ffcc3d1c4e53d6dc9babe846a39f508
Paul Canty <pcanty@greenough.com>
2018-01-05 21:00:01 -0800

    adding template to .html with mustache to make messaging scalable

--- public/js/index.js
+++ public/js/index.js
@@ -10,24 +10,39 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function ( message ) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $( '#message-template' ).html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    var li = $( '<li></li>' );
-    li.text(`${ message.from } ${ formattedTime }: ${ message.text }`);
+    $( '#messages' ).append(html);
 
-    $( '#messages' ).append(li);
+    // var li = $( '<li></li>' );
+    // li.text(`${ message.from } ${ formattedTime }: ${ message.text }`);
+
+    // $( '#messages' ).append(li);
 });
 
 socket.on('newLocationMessage', function ( message ) {
-    var li = $( '<li></li>' );
-    var a = $( '<a target="_blank">My Current Location</a>' );
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $( '#location-message-template' ).html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
+    $( '#messages' ).append(html);
 
-    li.text(`${ message.from } ${ formattedTime }: `);
-    a.attr('href', message.url);
-    li.append(a);
+    // var li = $( '<li></li>' );
+    // var a = $( '<a target="_blank">My Current Location</a>' );
 
-    $( '#messages' ).append(li);
+    // li.text(`${ message.from } ${ formattedTime }: `);
+    // a.attr('href', message.url);
+    // li.append(a);
+
 })
 
 $('#message-form').on('submit', function ( event ) {
